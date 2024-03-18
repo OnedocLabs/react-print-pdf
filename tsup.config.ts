9e7d9d981d1e6136fe17dddc6920b929ab1d8c66
Titouan Launay <titouan.launay@lleedpartners.com>
2024-03-18 16:24:40 -0700

    Updates build steps

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "tsup";
 import { RawPlugin } from "./build/raw";
+import plugin from "node-stdlib-browser/helpers/esbuild/plugin";
+import stdLibBrowser from "node-stdlib-browser";
 
 export default defineConfig({
   entry: ["src/index.ts", "src/mdx.ts", "src/server.ts", "src/client.ts"],
@@ -7,7 +9,18 @@ export default defineConfig({
   sourcemap: true,
   clean: false,
   format: ["cjs", "esm"],
+  platform: "neutral",
   dts: true,
-  esbuildPlugins: [RawPlugin()],
+  esbuildOptions: (options) => {
+    options.inject = ["node-stdlib-browser/helpers/esbuild/shim"];
+    options.define = {
+      url: "url",
+      tty: "tty",
+      fs: "fs",
+      path: "path",
+      process: "process",
+    };
+  },
+  esbuildPlugins: [RawPlugin(), plugin(stdLibBrowser)],
   bundle: true,
 });
