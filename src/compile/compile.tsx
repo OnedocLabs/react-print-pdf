fb3cb19923e0487e1344bb8ffe5fb123605f0ca2
Pierre Dorge <pierre.dorge@onedoclabs.com>
2024-04-29 17:32:25 +0200

    Removes console.log in compile function

--- src/compile/compile.tsx
+++ src/compile/compile.tsx
@@ -27,8 +27,6 @@ export const compile = async (
 
   const ReactDOMServer = await import("react-dom/server");
 
-  console.log(ReactDOMServer);
-
   let Element = (
     <>
       <CSS>{onedocStyles}</CSS>
