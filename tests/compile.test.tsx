0d88cf015746646c965fb55112059ab2b3e8587c
Titouan Launay <titouan.launay@lleedpartners.com>
2024-07-17 11:13:54 +0200

    Changes test to check for :is selector

--- tests/compile.test.tsx
+++ tests/compile.test.tsx
@@ -39,7 +39,6 @@ test("works with tailwind dark", async () => {
 
   const html = await compile(<TestComponent />);
 
-  console.log(html);
-
   expect(html).toContain("rgba(239, 68, 68");
+  expect(html).not.toContain(":is");
 });
